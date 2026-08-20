6155a4042c0135d4e26abcbb85590d0a430cf9f7
NotADeckChair <prime7three@outlook.com>
2026-08-20 20:51:33 +0200

    redactpdf: sha256 for v0.3.1.0804.1809

--- Formula/redactdpdf.rb
+++ Formula/redactdpdf.rb
@@ -14,7 +14,7 @@ class Redactdpdf < Formula
   desc     "PDF metadata scrubber, linearizer and timestamp setter"
   homepage "https://github.com/NotADeckChair/redactdPDF"
   url      "https://github.com/NotADeckChair/redactdPDF/archive/refs/tags/v0.3.1.0804.1809.tar.gz"
-  sha256   "REPLACE_WITH_SHA256_OF_RELEASE_ARCHIVE"
+  sha256   "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license  "Apache-2.0"
   version  "0.3.1.0804.1809"
 
